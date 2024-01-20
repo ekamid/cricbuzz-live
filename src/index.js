0897046d3c81d2eb8ecd6a1572e27153e2d558de
ekamid <ebrahimkhalilamidbd@gmail.com>
2024-01-20 12:56:44 +0600

    checking vercel in production with a new app config

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-const http = require('http');
-const { App } = require('./config/app');
-const { env } = require('./config/env');
-const { httpHandle } = require('./core/modules/httpHandle');
+const express = require("express");
 
-function bootstrap() {
-    const port = env.APP_PORT;
+const app = express();
 
-    // create express app
-    const app = new App().create();
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
 
-    const server = http.createServer(app);
+app.listen(5000, () => {
+    console.log("Running on port 5000.");
+});
 
-    // http handle
-    const { onError, onListening } = httpHandle(server, port);
-
-    // run server listen
-    server.listen(port);
-    server.on('error', onError);
-    server.on('listening', onListening);
-}
-
-bootstrap();
+// Export the Express API
+module.exports = app;
